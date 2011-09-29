36f99af5e2c983b8667d094dd01fbd38eb815d9e
Alex Cros <alexcroscuadros@gmail.com>
2011-09-29 22:41:27 +0200

    finished country mapping

--- web-model/src/main/java/org/pisit/web/model/master/Country.java
+++ web-model/src/main/java/org/pisit/web/model/master/Country.java
@@ -1,7 +1,17 @@
 package org.pisit.web.model.master;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.pisit.web.model.entity.Identity;
 
+@Entity
+@AttributeOverride(name = "id", column = @Column(name = "CITY_ID", nullable = false))
 public class Country extends Identity {
 
     /**
@@ -9,8 +19,17 @@ public class Country extends Identity {
      */
     private static final long serialVersionUID = 1845183156848502817L;
 
+    @Basic
     private String name;
 
+    @Override
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_COUNTRY")
+    public Long getId() {
+	return this.id;
+    }
+
+    @Column(name = "COUNTRY_NAME", length = 128, nullable = false, unique = true)
     public String getName() {
 	return this.name;
     }
